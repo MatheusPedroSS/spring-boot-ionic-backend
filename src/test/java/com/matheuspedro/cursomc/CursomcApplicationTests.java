08de8e105003cd077d2d1fe39d23264053fc5521
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-01-31 16:38:40 -0300

    Projeto criado

---
+++ src/test/java/com/matheuspedro/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuspedro.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
