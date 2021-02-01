62ee06a51b1892bc625bca581c581031467b25f7
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-02-01 18:14:38 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/matheuspedro/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.matheuspedro.cursomc.repositories;
+
+import com.matheuspedro.cursomc.domain.Produto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto , Integer> {
+
+    
+
+}
